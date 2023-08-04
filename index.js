335a0c77715241320f8dc651cc7c5b6262ebf792
Mohsen Shirazi Nalband <mohsensh4792@gmail.com>
2023-08-04 04:14:19 -0400

    Updated the for loop inside of renderCharacter()
    The new for loop so it uses a value from the new diceRoll array to render out the dice so the  wizard's dice have values of 3, 1 and 4, and the orc's single dice has a value of 2.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hero = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: 6,
+    diceRoll: [3, 1, 4],
     diceCount: 3
 }
 
@@ -12,7 +12,7 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: 4,
+    diceRoll: [2],
     diceCount: 1
 }
 
@@ -24,7 +24,7 @@ function renderCharacter(data){
         let diceHtml = ``;
 
         for(let i = 0; i < diceCount; i++){
-            diceHtml += `<div class="dice">6</div>`;
+            diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
         }
 
         document.getElementById(`${data.elementId}`).innerHTML = `<div class="character-card">
