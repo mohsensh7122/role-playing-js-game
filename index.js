8d8a5b964062a2d287eb7ec3de86cd6918b4daf2
Mohsen Shirazi Nalband <mohsensh4792@gmail.com>
2023-08-03 23:08:38 -0400

    Added the hero and monster objects.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 6
+}
+
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4
+}
+
+
 document.getElementById('hero').innerHTML = `
     <div class="character-card">
         <h4 class="name"> Wizard </h4>
