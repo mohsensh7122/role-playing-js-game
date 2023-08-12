a9e6517c8f66d4e7c66b6594b5017b923a926323
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-08-12 14:04:32 -0400

    Added the healthBar variable to the game.

--- Character.js
+++ Character.js
@@ -54,6 +54,7 @@ function Character(data) {
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${this.diceArray}
                 </div>
